2c3547f41852674023c1c445c05cc6fd6d6eb6ee
amnzmj <amunoz31965@ucq.edu.mx>
2025-06-12 11:05:19 -0600

    bootscreen y statusscreen modificado

--- _Statusscreen.h
+++ _Statusscreen.h
@@ -34,7 +34,7 @@
 //
 // Status Screen Logo bitmap
 //
-#define STATUS_LOGO_Y     8
+//#define STATUS_LOGO_Y     8
 #define STATUS_LOGO_WIDTH 128
 
 const unsigned char status_logo_bmp[] PROGMEM = {
