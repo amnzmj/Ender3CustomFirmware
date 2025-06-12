c31e5e31cb94844e1c531f3ef59abc0477597801
amnzmj <amunoz31965@ucq.edu.mx>
2025-06-11 22:21:24 -0600

    1407 S_CURVE_ACCELERATION localizado

--- Configuration.h
+++ Configuration.h
@@ -1404,7 +1404,8 @@
  *
  * See https://github.com/synthetos/TinyG/wiki/Jerk-Controlled-Motion-Explained
  */
-#define S_CURVE_ACCELERATION
+#define S_CURVE_ACCELERATION 
+
 
 //===========================================================================
 //============================= Z Probe Options =============================
